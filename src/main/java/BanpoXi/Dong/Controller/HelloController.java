f3c121f1b024f30d919fca3ef9d7c471e106361c
dong <ultraraptor@naver.com>
2021-03-03 16:05:21 +0900

    8강 진행중 한번 : 영한이형은 최고야!!

--- src/main/java/BanpoXi/Dong/Controller/HelloController.java
+++ src/main/java/BanpoXi/Dong/Controller/HelloController.java
@@ -1,7 +1,10 @@
 package BanpoXi.Dong.Controller;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
@@ -11,5 +14,41 @@ public class HelloController {
         System.out.println("hewllwe");
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "Hello-temeplete";
+    }
+
+
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;//hello spring
+    }
+
+
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello  = new Hello();
+        hello.setName(name);
+        return hello;//문자가 아닌 객체를 넘긴다!
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;// + "Dong!";
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
