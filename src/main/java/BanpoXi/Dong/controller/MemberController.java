c5206e565fdffda718e3117a740ca51be7555786
dong <ultraraptor@naver.com>
2021-03-18 00:36:37 +0900

    회원관리에ㅖ제 67% 구간

--- src/main/java/BanpoXi/Dong/controller/MemberController.java
+++ src/main/java/BanpoXi/Dong/controller/MemberController.java
@@ -5,8 +5,11 @@ import BanpoXi.Dong.repository.MemberRepository;
 import BanpoXi.Dong.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -23,17 +26,32 @@ public class MemberController {
     }
 
     @GetMapping("/members/new")
+    //getmapping : 데이터 조회 시 많이 사용된다.
     public String createForm() {
         return "members/createMemberForm";
     }
 
+    //postMapping : form 같은데다가 데이터를 넣어서 보낼때 많이 씀
     @PostMapping("/members/new")
     public String createUser(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
 
+
+        System.out.println(member.getName());
         memberService.join(member);
 
         return "redirect:/";
+    }
+
+    //동일한 url 에 get, post 둘다 사용중인데 둘다 쓸 수 있고, 웹페이지 요청유형에 따라 알아서 잘 호출된
+
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> memberList = memberService.findMembers();
+        model.addAttribute("members",memberList);
+
+        return "members/memberList";
     }
 }
